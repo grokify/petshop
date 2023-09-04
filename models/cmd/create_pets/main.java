86cf76cf9c34898562d17612a7ea609389c9004a
John Wang <johncwang@gmail.com>
2023-09-04 08:31:47 -0700

    style: `models/cmd/create_pets`: convert tabs

--- models/cmd/create_pets/main.java
+++ models/cmd/create_pets/main.java
@@ -26,19 +26,19 @@ class mysql_example {
         }
         System.out.println("Starting MySQL Petstore insert example.");
 
-		Integer iterations = Integer.parseInt(args[5]);
+        Integer iterations = Integer.parseInt(args[5]);
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(baos);
 
-		String usessl = "&useSSL=false";
-		//usessl = "";
+        String usessl = "&useSSL=false";
+        usessl = "";
 
-		// useSSL=false is to connect to 5.7 server https://stackoverflow.com/questions/67332909/why-can-java-not-connect-to-mysql-5-7-after-the-latest-jdk-update-and-how-should
+        // useSSL=false is to connect to 5.7 server https://stackoverflow.com/questions/67332909/why-can-java-not-connect-to-mysql-5-7-after-the-latest-jdk-update-and-how-should
         ps.printf("jdbc:mysql://%s:%s/%s?user=%s&password=%s%s", args[0], args[1], args[2], args[3], args[4], usessl);
         String jdbcdsn = baos.toString();
 
-		System.out.println("DSN: "+jdbcdsn);
+        System.out.println("DSN: "+jdbcdsn);
 
         // Connection conn = null;
         try {
@@ -53,9 +53,9 @@ class mysql_example {
 
             PreparedStatement preparedStmt = conn.prepareStatement(query);
 
-       		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
-        	ZonedDateTime dt0 = ZonedDateTime.now();
-       		System.out.println(fmt.format(dt0));
+            DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZZZZZ");
+            ZonedDateTime dt0 = ZonedDateTime.now();
+               System.out.println(fmt.format(dt0));
 
             for (int i = 0; i < iterations; i++) {
                 preparedStmt.setString (1, "Java A" + String.valueOf(i));
@@ -65,20 +65,20 @@ class mysql_example {
                 preparedStmt.execute();
             }
 
-        	ZonedDateTime dt1 = ZonedDateTime.now();
-        	System.out.println(fmt.format(dt1));
+            ZonedDateTime dt1 = ZonedDateTime.now();
+            System.out.println(fmt.format(dt1));
 
-        	Duration dur = Duration.between(dt0,dt1);
-        	long durms = dur.toMillis();
-        	long durmn = TimeUnit.MILLISECONDS.toMinutes(durms)*60;
-        	// String time = String.format("%02d hours, %02d min, %02d sec",
-        	String time = String.format("%02d:%02d:%02d",
-            	TimeUnit.MILLISECONDS.toHours(durms),
-            	TimeUnit.MILLISECONDS.toMinutes(durms) - TimeUnit.MILLISECONDS.toHours(durms) * 60,
-            	TimeUnit.MILLISECONDS.toSeconds(durms) - durmn);
-        	System.out.println("time elapsed = " + time);
-			System.out.println("time elapsed = "+ dur.toString());
-			System.out.println("system: "+ args[0]);
+            Duration dur = Duration.between(dt0,dt1);
+            long durms = dur.toMillis();
+            long durmn = TimeUnit.MILLISECONDS.toMinutes(durms)*60;
+            // String time = String.format("%02d hours, %02d min, %02d sec",
+            String time = String.format("%02d:%02d:%02d",
+                TimeUnit.MILLISECONDS.toHours(durms),
+                TimeUnit.MILLISECONDS.toMinutes(durms) - TimeUnit.MILLISECONDS.toHours(durms) * 60,
+                TimeUnit.MILLISECONDS.toSeconds(durms) - durmn);
+            System.out.println("time elapsed = " + time);
+            System.out.println("time elapsed = "+ dur.toString());
+            System.out.println("system: "+ args[0]);
 
             conn.close();
         } catch (SQLException ex) {
